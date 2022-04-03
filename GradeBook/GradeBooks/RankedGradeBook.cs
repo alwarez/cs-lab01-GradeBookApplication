06e357e23637cdaf56273304af947439c7e1ddd6
alwarez <fkrwiec@gmail.com>
2022-04-03 13:50:36 +0200

    4. 3x Override in RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,9 +6,40 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook:BaseGradeBook
     {
-        public RankedGradeBook(string name):base(name)
+        public RankedGradeBook(string name):base(name){
+            Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
         {
-            ConvertToGradeBook("Ranked");
+            if (Students.Count < 5)
+
+                throw new InvalidCastException();
+
+            if (averageGrade > 80){
+                return 'A';
+            }
+            if (averageGrade > 60){
+                return 'B';
+            }
+            if (averageGrade > 40){
+                return 'C';
+            }
+            if (averageGrade > 20){
+                return 'D';
+            }
+                return 'F';
+        }
+        public override void CalculateStatistics(){
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name){
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStudentStatistics(name);
         }
     }
 }
